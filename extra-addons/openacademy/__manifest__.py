353a27bc787adb206430a4d9222fee8b5523bf64
Samuel <sgardonprieto@danielcastelao.org>
2024-03-05 10:40:51 +0100

    Primer commit

---
+++ extra-addons/openacademy/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "OpenAcademy",
+
+    'summary': """
+        Abierta academia modulo""",
+
+    'description': """
+        Abierta academia modulo por Samuel Gardón Prieto
+    """,
+
+    'author': "Samuel",
+    'website': "https://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.5',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
